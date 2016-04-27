0c2ea1ca27cb42dab6f5c3198063e688430b79ae
mariakhair <mariakhair@Marias-MacBook-Pro.local>
2016-04-26 18:18:35 -0700

    first commit

---
+++ ice_cream_shop/assets/js/app.js
@@ -0,0 +1,15 @@
+//initializing jQuery
+$(document).ready(function(){
+	
+	
+	//creating the parallax effect
+	var scene = document.getElementById('body');
+	var parallax = new Parallax(scene);
+
+
+	//animation
+	$('#main').addClass('animated tada');
+
+});
+
+
